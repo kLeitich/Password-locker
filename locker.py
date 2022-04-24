c3837b21ba34b655fec8ea616ff881ba1e462bef
kLeitich <kevleitich@gmail.com>
2022-04-24 04:33:25 +0300

    adding locker to the repo

---
+++ locker.py
@@ -0,0 +1,28 @@
+from click import pass_context
+
+
+class User:
+
+    user_list=[{"username":"kevin","password":"1421"},]
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        User.user_list.append(self)
+
+    def delete_user(self):
+        User.user_list.remove(self)
+    
+    @classmethod
+    def find_by_user(cls,username,password):
+        for user in cls.user_list:
+            if user.username == username:
+                if user.password == password:
+                    print("You are logged in "+ username)
+                else:
+                    print("Try again")
+            else:
+                print("username not found")
+    
